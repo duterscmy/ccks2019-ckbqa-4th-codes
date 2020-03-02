9c6ef8198bb728dfe4f12c0bae4089300974f673
Caomingyu <1054527636@qq.com>
2020-03-02 16:58:35 +0800

    Add files via upload

--- src/kb.py
+++ src/kb.py
@@ -12,6 +12,7 @@ driver = GraphDatabase.driver("bolt://localhost:7687")
 session = driver.session()
 begintime = time.time()
 session.run('MATCH (n) OPTIONAL MATCH (n)-[r]->() RETURN count(n.name) + count(r)')
+#session.run('CREATE INDEX ON:Entity(name)')
 endtime = time.time()
 print ('start neo4j and match all entities,the time is %.2f'%(endtime-begintime))
 
@@ -19,11 +20,10 @@ def GetRelationPaths(entity):
     '''根据实体名，得到所有1跳2跳关系list,2d-list'''
 
     cql_1 = "match (a:Entity)-[r1:Relation]-() where a.name=$name return DISTINCT r1.name"
-    cql_2 = "match (a:Entity)-[r1:Relation]->()-[r2:Relation]->() where a.name=$name return DISTINCT r1.name,r2.name"
-    #cql_2 = "match (a:Entity)-[r1:Relation]-()-[r2:Relation]->() where a.name=$name return DISTINCT r1.name,r2.name"
+    cql_2 = "match (a:Entity)-[r1:Relation]-()-[r2:Relation]->() where a.name=$name return DISTINCT r1.name,r2.name"
     rpaths1 = []
-    res = session.run(cql_1,name=entity)
-    for record in res:
+    res = session.run(cql_1,name=entity)#一个多个record组成的集合
+    for record in res:#每个record是一个key value的有序序列
         rpaths1.append([record['r1.name']])
     rpaths2 = []
     res = session.run(cql_2,name=entity)
@@ -31,10 +31,21 @@ def GetRelationPaths(entity):
         rpaths2.append([record['r1.name'],record['r2.name']])
     return rpaths1+rpaths2
 
+def GetRelationPathsSingle(entity):
+    '''根据实体名，得到所有1跳2跳关系list,2d-list'''
+
+    cql_1 = "match (a:Entity)-[r1:Relation]-() where a.name=$name return DISTINCT r1.name"
+    rpaths1 = []
+    res = session.run(cql_1,name=entity)#一个多个record组成的集合
+    for record in res:#每个record是一个key value的有序序列
+        rpaths1.append([record['r1.name']])
+    return rpaths1
+
 def GetRelations_2hop(entity):
-    cql_2 = "match (a:Entity)-[r1:Relation]-()-[r2:Relation]->() where a.name=$name return DISTINCT r1.name,r2.name"
+    '''根据实体名，得到两跳内的所有关系dic'''
+    cql= "match (a:Entity)-[r1:Relation]-()-[r2:Relation]->() where a.name=$name return DISTINCT r1.name,r2.name"
     rpaths2 = []
-    res = session.run(cql_2,name=entity)
+    res = session.run(cql,name=entity)
     for record in res:
         rpaths2.append([record['r1.name'],record['r2.name']])
     dic = {}
@@ -43,57 +54,24 @@ def GetRelations_2hop(entity):
             dic[r] = 0
     return dic
 
-def GetRelationPath_BetweenEntitys(e1,e2):
-    '''
-    给定两个实体，返回二者间的关系序列列表
-    '''
-    cql_2 = "match (a:Entity)-[r1:Relation]-()-[r2:Relation]-(b:Entity) where a.name=$name and b.name=$name2 return DISTINCT r1.name,r2.name"
-    rpaths = []
-    res = session.run(cql_2,name=e1,name2=e2)
+def GetRelationNum(entity):
+    '''根据实体名，得到与之相连的关系数量，代表实体在知识库中的流行度'''
+    cql= "match p=(a:Entity)-[r1:Relation]-() where a.name=$name return count(p)"
+    res = session.run(cql,name=entity)
+    ans = 0
     for record in res:
-        rpaths.append([record['r1.name'],record['r2.name']])
-    return rpaths
+        ans = record.values()[0]
+    return ans
 
-def Get_Tuples_class1(entity):
-    '''根据实体名，得到所有1跳'''
-
-    cql_1 = "match (a:Entity)-[r1:Relation]-() where a.name=$name return DISTINCT r1.name"
-    rpaths = []
-    res = session.run(cql_1,name=entity)
+def GetRelationNumsig(entity):
+    '''根据实体名，得到一跳内的不同关系个数,重要度'''
+    cql= "match p=(a:Entity)-[r1:Relation]-() where a.name=$name return DISTINCT r1.name"
+    rpath = []
+    res = session.run(cql,name=entity)
     for record in res:
-        rpaths.append([record['r1.name']])
-    return rpaths
-
-def Get_Tuples_class2(entity):
-    '''根据实体名，得到固定逻辑形式的2跳关系list,2d-list'''
-
-    cql_2 = "match (a:Entity)-[r1:Relation]->(b:Entity)-[r2:Relation]->() where a.name=$name return DISTINCT r1.name,r2.name"
-    rpaths = []
-    res = session.run(cql_2,name=entity)
-    for record in res:
-        rpaths.append([record['r1.name'],record['r2.name']])
-    return rpaths
-
-def Get_Tuples_class3(entity):
-    '''根据实体名，得到固定逻辑形式的2跳关系list,2d-list'''
-
-    cql_2 = "match (a:Entity)<-[r1:Relation]-(b:Entity)-[r2:Relation]->() where a.name=$name return DISTINCT r1.name,r2.name"
-    rpaths = []
-    res = session.run(cql_2,name=entity)
-    for record in res:
-        rpaths.append([record['r1.name'],record['r2.name']])
-    return rpaths
-
-def Get_Tuples_class4(entity):
-    '''根据实体名，得到固定逻辑形式的2跳关系list,2d-list'''
-
-    cql_2 = "match (a:Entity)-[r1:Relation]->(b:Entity)<-[r2:Relation]-() where a.name=$name return DISTINCT r1.name,r2.name"
-    rpaths = []
-    res = session.run(cql_2,name=entity)
-    for record in res:
-        rpaths.append([record['r1.name'],record['r2.name']])
-    return rpaths
-
+        rpath.append(record['r1.name'])
+    return len(set(rpath))
+                      
 def GetTwoEntityTuple(e1,r1,e2):
     cql = "match (a:Entity)-[r1:Relation]-(b:Entity)-[r2:Relation]-(c:Entity) where a.name=$e1n and r1.name=$r1n and c.name=$e2n return DISTINCT r2.name"
     tuples = []
@@ -104,13 +82,8 @@ def GetTwoEntityTuple(e1,r1,e2):
 
 
 if __name__ == '__main__':
-    def runcql3(tx,cql,ename1,ename2,rname1,rname2):#用于查询实体直接相连的关系
-        l = []
-        for record in tx.run(cql,name1=ename1,name2=ename2,name3=rname1,name4=rname2):
-            l.append([record['b.name']])
-        return l
-    cql = "match (a:Entity)-[r1:Relation]-(b:Entity)-[r2:Relation]-(c:Entity) where a.name=$name1 and c.name=$name2 and r1.name=$name3 and r2.name=$name4 return b.name"
-    res = session.run(cql,name1='<复旦大学>',name2='<政治家>',name3='<毕业院校>',name4='<职业>')
-    l = [record['b.name'] for record in res]
-    print (l)
-    session.close()
+    
+    sql = "match (a:Entity)-[r1:Relation]-(c)-[r2:Relation]-(b:Entity) where a.name=$ename1 and r1.name=$rname1 and r2.name=$rname2 and b.name=$ename2 return c.name"
+    res = session.run(sql,ename1='<维力医疗>',rname1='<公司高管>',rname2='<硕士学位>',ename2='<高分子材料专业>')
+    ans = [record['c.name'] for record in res]
+    print(ans)
